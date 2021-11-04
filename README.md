4cde7254e4924fdde04a3c64b598a493103a1a69
Maciej Krüger <mkg20001@gmail.com>
2021-11-04 14:42:44 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# yt-music-playback-speed
+
+A tampermonkey script to modify playback speed on youtube music
+
+![screenshot](./screenshot.png)
+
+# How to use
+
+Install Tampermonkey Extension
+- How to https://www.youtube.com/watch?v=8tyjJD65zws
+- Download https://www.tampermonkey.net/
---
+++ yt-music-playback-speed.js
@@ -0,0 +1,102 @@
+// ==UserScript==
+// @name         YouTube Music Playback Speed
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Allows to adjust the playback speed on youtube music web
+// @author       Maciej Krüger (mkg20001@gmail.com)
+// @match        https://music.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?domain=music.youtube.com
+// @grant        none
+// ==/UserScript==
+
+// Contrib at https://github.com/mkg20001/yt-music-playback-speed
+
+(function() {
+    'use strict';
+
+    let playbackSpeed = 1
+
+    const log = console.log.bind(console)
+
+    function applySpeed() {
+        if (!v) return
+        log('reset rate: was %o, is %o', v.playbackRate, playbackSpeed)
+        v.playbackRate = playbackSpeed
+    }
+
+    function storeSpeed(speed) {
+        log('ratechange: was %o, is %o', speed, v.playbackRate)
+        playbackSpeed = speed
+        if (s) {
+            s.textContent = playbackSpeed.toFixed(2) + 'x'
+        }
+    }
+
+    function signBtn(sign, fnc) {
+        const btn = document.createElement('p')
+        btn.style.fontWeight = 'bold'
+        btn.style.fontSize = '4em'
+        btn.style.margin = '0.2em 0.4em'
+        btn.style.cursor = 'pointer'
+        btn.textContent = sign
+        btn.onclick = fnc
+
+        return btn
+    }
+
+    let v
+    let s
+
+    function adjRate(amount) {
+        storeSpeed(parseFloat((playbackSpeed + amount).toFixed(10), 10))
+        applySpeed()
+    }
+
+    function registerBar() {
+        const bar = document.querySelector('#left-controls')
+
+        if (!bar) return alert('yt music playback speed had an error while intializing: register couldnt find bar')
+
+        const div = document.createElement('div')
+        div.style.display = 'flex'
+        div.style.alignItems = 'center'
+
+        s = document.createElement('p')
+        s.style.fontSize = '2em'
+        storeSpeed(playbackSpeed)
+
+        div.append(signBtn('-', (e) => {
+          e.preventDefault()
+          adjRate(-0.1)
+        }))
+        div.append(s)
+        div.append(signBtn('+', (e) => {
+          e.preventDefault()
+          adjRate(0.1)
+        }))
+
+        bar.append(div)
+    }
+
+    function register() {
+        v = document.querySelector('video')
+
+        log('registering')
+
+        v.addEventListener('ratechange', (e) => {
+            storeSpeed(v.playbackRate)
+        })
+
+        v.addEventListener('loadeddata', (e) => {
+            applySpeed()
+        })
+
+        registerBar()
+    }
+
+    const intv = setInterval(() => {
+        if (!document.querySelector('video')) return // didn't load yet
+        register()
+        clearInterval(intv)
+    }, 100)
+ })();
